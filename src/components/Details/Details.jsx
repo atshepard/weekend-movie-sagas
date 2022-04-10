7c15830fa75827cb75530905a9d0f7e95a4eb94e
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-04-10 08:04:15 -0500

    fixes render issue on detailis page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { Card, CardHeader, CardMedia, Button, CardContent } from '@mui/material'
+import { useEffect } from "react";
 
 function Details() {
     const movieArray = useSelector(store => store.details);
@@ -8,7 +9,11 @@ function Details() {
     const genres = movieArray.map(movie => movie.name);
     const history = useHistory();
 
-    // console.log('logging movieArray, movie: ', movieArray, movie);
+    // useEffect(() => {
+    //     console.log('logging movieArray, movie: ', movieArray, movie);
+    // }, []);
+
+    console.log('logging movieArray, movie: ', movieArray, movie);
 
     return (<>
         <Button onClick={() => history.push('/')}>Return to Movie List</Button>
@@ -18,14 +23,14 @@ function Details() {
                 <CardMedia
                     component="img"
                     height="500px"
-                    image={movie.poster}
-                    alt={movie.title}
+                    image={movie?.poster}
+                    alt={movie?.title}
                 />
                 <CardHeader>
-                    <h3>{movie.title}</h3>
+                    <h3>{movie?.title}</h3>
                 </CardHeader>
                 <CardContent>
-                    <p>{movie.description}</p>
+                    <p>{movie?.description}</p>
                     <p>GENRES:</p>
                     {genres.map((genre, i) => {
                         return (<li key={i}>{genre}</li>)
