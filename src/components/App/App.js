3f8581db15ba4ad0d20463ecaa81b6a4fd1a6807
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-04-10 08:42:07 -0500

    adds theme and theme provider

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,45 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList';
 import Details from '../Details/Details';
 import Header from '../Header/Header.jsx';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#aa9d94',
+      },
+      secondary: {
+        main: '#bbada1',
+      },
+      info: {
+        main: '#20498a',
+      },
+    },
+  });
+
+
   return (
-    <div className="App">
-      <Header />
-      <Router>        
-        <Route path="/" exact>
-          <MovieList />
-        </Route>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header />
+        <Router>
+          
+          <Route path="/" exact>
+            <MovieList />
+          </Route>
 
           <Route path="/details">
             <Details />
           </Route>
 
-        {/* Add Movie page */}
-      </Router>
-    </div>
+          {/* Add Movie page */}
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
