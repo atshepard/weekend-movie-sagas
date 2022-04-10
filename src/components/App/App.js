d3e81ac9d5a095f52d66b36e29918186b5928d65
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-04-09 20:53:53 -0500

    adds header background

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
-import MovieList from '../MovieList/MovieList'
-import Details from '../Details/Details'
+import MovieList from '../MovieList/MovieList';
+import Details from '../Details/Details';
+import Header from '../Header/Header.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>        
         <Route path="/" exact>
           <MovieList />
